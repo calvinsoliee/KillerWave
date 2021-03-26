0e92e4d5dae06d15acd0a6b9fcc7b042a6a8bea9
calvinsoliee <77906888+calvinsoliee@users.noreply.github.com>
2021-03-26 12:59:14 -0700

    Completed Chapter 5 & 6

--- KillerWave Game/Assets/Resources/Scripts/PlayerSpawner.cs
+++ KillerWave Game/Assets/Resources/Scripts/PlayerSpawner.cs
@@ -4,26 +4,43 @@ public class PlayerSpawner : MonoBehaviour
 {
 	SOActorModel actorModel;
 	GameObject playerShip;
+	bool upgradedShip = false;
 
 	void Start()
 	{
 		CreatePlayer();
+		GetComponentInChildren<Player>().enabled = true;
 	}
 
 	void CreatePlayer()
 	{
-		//CREATE PLAYER
-		actorModel = Object.Instantiate(Resources.Load("Scripts/ScriptableObject/Player_Default")) as SOActorModel;
-		playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
-		playerShip.GetComponent<Player>().ActorStats(actorModel);
+		//been shopping
+		if(GameObject.Find("UpgradedShip"))
+        {
+			upgradedShip = true;
+        }
+		
+		//not shopped or died 
+		//default ship build
+		if (!upgradedShip || GameManager.Instance.Died)
+        {
+			GameManager.Instance.Died = false;
+			actorModel = Object.Instantiate(Resources.Load("Scripts/ScriptableObject/Player_Default")) as SOActorModel;
+			playerShip = GameObject.Instantiate(actorModel.actor, this.transform.position, Quaternion.Euler(270, 180, 0)) as GameObject;
+			playerShip.GetComponent<IActorTemplate>().ActorStats(actorModel);
+        }
+		//apply the shop upgrades
+		else
+        {
+			playerShip = GameObject.Find("UpgradedShip");
+        }
 
-		//SET PLAYER UP
 		playerShip.transform.rotation = Quaternion.Euler(0, 180, 0);
 		playerShip.transform.localScale = new Vector3(60, 60, 60);
+		playerShip.GetComponentInChildren<ParticleSystem>().transform.localScale = new Vector3(25, 25, 25);
 		playerShip.name = "Player";
 		playerShip.transform.SetParent(this.transform);
 		playerShip.transform.position = Vector3.zero;
-		playerShip.GetComponentInChildren<ParticleSystem>().transform.localScale = new Vector3(25, 25, 25);
 	}
 }
 
